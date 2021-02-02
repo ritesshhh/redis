adc63d0548aa081846ce1d899017f77e3d1da2a6
Ritesh Kumar <ritesh.kumar@cyware.com>
2021-02-02 16:30:18 +0530

    initial commit

---
+++ get_redis_queues_with_sizes.py
@@ -0,0 +1,29 @@
+import time
+from django_redis import get_redis_connection
+
+
+def take_redis_snap(**kwargs):
+    print("<=== take_redis_snap running ===>")
+    length = 0
+    r = get_redis_connection("default")
+    keys = r.keys('*')
+
+    scard_list = r.keys("*es_thre*") # data types that uses scard command
+    main_queues = r.keys("high*") # data types that uses llen command
+    main_queues.extend(r.keys("low*"))
+    main_queues.extend(r.keys("high*"))
+    main_queues.extend(r.keys("critical*"))
+
+    data = ""
+    for key in keys:
+        size = 0
+        if key in scard_list:
+            size = r.scard(key)
+        elif key in main_queues:
+            size = r.llen(key)
+        if size > int(length):
+            data = data + f"{str(key)}, size: {size} \n"
+    if data:
+        data = data + f"time : {time.asctime()} \n\n"
+        with open('redis_snap.txt', 'a') as f:
+            f.write(data)
